3c3c7b90d49be15e100de9f40d45344a7bddfeac
tsur0evv <storm00606@gmail.com>
2025-12-26 16:06:05 +0300

    Add files via upload

---
+++ server/error/ApiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error{
+    constructor(status, message) {
+        super(); // вызов род. конструктор с помощью super
+        this.status = status
+        this.message = message
+    }
+    // статические функции - это функции которые можно вызывать без создания объекта
+    static badRequest(message) {
+        return new ApiError(404, message)
+    }
+
+    static internal(message) {
+        return new ApiError(500, message)
+    }
+
+    static forbidden(message) {
+        return new ApiError(403, message)
+    }
+}
+
+module.exports = ApiError
+
+
+
+
+
